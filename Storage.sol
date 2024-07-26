f8c75b8f7b37878a26c54ea62f4f31e131065b77
Víctor Muñoz <53861440+vmmunoza@users.noreply.github.com>
2024-07-26 02:28:28 +0200

    Add files via upload

---
+++ Storage.sol
@@ -0,0 +1,17 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.24;
+
+// Uncomment this line to use console.log
+// import "hardhat/console.sol";
+
+contract SimpleStorage {
+    uint storedData;
+
+    function set(uint x) public {
+        storedData = x;
+    }
+
+    function get() public view returns (uint) {
+        return storedData;
+    }
+}
